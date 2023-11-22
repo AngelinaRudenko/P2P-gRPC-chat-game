18e6cfe787bfb6be1c4b78eb5ff43f734bac46b6
Angelina Rudenko <angel.rudenko.777@gmail.com>
2023-11-22 19:28:13 +0100

    grpc web app added

---
+++ src/P2P.Chat.Node/Services/ChatService.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Core;
+using Proto;
+
+namespace P2P.Chat.Node.Services;
+
+public class ChatService : Proto.ChatService.ChatServiceBase
+{
+    public override Task<ChatResponse> Chat(ChatRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new ChatResponse { IsOk = true });
+    }
+}
